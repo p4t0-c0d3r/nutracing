ab4377f933c885b5301441e46faa51051f39ba67
Patricio <p.villanueva.fuentes@gmail.com>
2021-02-27 00:20:56 -0300

    Tabla de desayuno

--- src/components/Nutracing.js
+++ src/components/Nutracing.js
@@ -3,45 +3,139 @@ import React from 'react';
 export const Nutracing = () => {
     
     return(
-        <table>
-            <thead>
-                <tr>
-                    <th>Horario</th>
-                    <th>Comida</th>
-                    <th>Tipo</th>
-                    <th>Cantidad</th>
-                    <th>Unidad</th>
-                    <th>Opciones</th>
-                </tr> 
-           </thead>
-           <tbody>
-                <tr>
-                    <td rowSpan="2">09:00</td>
-                    <td rowSpan="2">DESAYUNO</td>
-                    <td rowSpan="2">LACTEO</td>
-                    <td>120</td>
-                    <td>gr</td>
-                    <td>yogurt protein</td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>tazón</td>
-                    <td>leche descremada con algo</td>
-                </tr>
-                <tr>
-                    <td rowSpan="2">11:00</td>
-                    <td rowSpan="2">COLACION 1</td>
-                    <td rowSpan="2">LACTEO</td>
-                    <td>120</td>
-                    <td>gr</td>
-                    <td>yogurt protein</td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>tazón</td>
-                    <td>leche descremada con algo</td>
-                </tr>
-           </tbody>
-        </table>
+        <>
+            <ol>
+                <li>FECHA</li>
+                <li>PESO</li>
+                <li>ESTATURA</li>
+                <li>CINTURA</li>
+                <li>IMC</li>
+                <li>DIAGNOSTICO</li>
+                <li>EDAD</li>
+                <li>GRASA</li>
+                <li>MUSCULO</li>
+            </ol>
+            
+            <table>
+            <tbody>
+                    <tr>
+                        <td rowSpan="21">09:00</td>
+                        <td rowSpan="21">DESAYUNO</td>
+                        <td rowSpan="4">LACTEOS</td>
+                        <td>120</td>
+                        <td>gr</td>
+                        <td>soprole, lonco leche</td>
+                        <td>yogurt protein</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="3">1</td>
+                        <td rowSpan="3">tazon</td>
+                        <td rowSpan="3">-</td>
+                        <td>leche cultivada descremada diet, light o cero</td>
+                    </tr>
+                    <tr>
+                        <td>leche descremada con sabor</td>
+                    </tr>
+                    <tr>
+                        <td>leche descremada con café o té + endulzante</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="7">CEREALES</td>
+                        <td>2</td>
+                        <td>reb</td>
+                        <td>vollkorn, casero, panaderia</td>
+                        <td>pan de molde integral</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="2">4</td>
+                        <td rowSpan="2">cda</td>
+                        <td>-</td>
+                        <td>avena sin azucar</td>
+                    </tr>
+                    <tr>
+                        <td>gran costa</td>
+                        <td>cereal sin azucar</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>uni</td>
+                        <td>-</td>
+                        <td>galleta de salvado</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="2">2</td>
+                        <td rowSpan="2">uni</td>
+                        <td>-</td>
+                        <td>pan pita integral</td>
+                    </tr>
+                    <tr>
+                        <td>-</td>
+                        <td>wrap integral</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>uni</td>
+                        <td>panaderia</td>
+                        <td>marraqueta CONDICION1</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="3">PROTEINA</td>
+                        <td rowSpan="1">1</td>
+                        <td rowSpan="1">reb</td>
+                        <td rowSpan="1">-</td>
+                        <td>jamón de pavo</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="1">2</td>
+                        <td rowSpan="1">reb</td>
+                        <td rowSpan="1">-</td>
+                        <td>quesillo</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="1">1</td>
+                        <td rowSpan="1">uni</td>
+                        <td rowSpan="1">-</td>
+                        <td>huevo</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="6">GRASA</td>
+                        <td rowSpan="1">1/2</td>
+                        <td rowSpan="1">uni</td>
+                        <td rowSpan="1">-</td>
+                        <td>palta</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="1">3</td>
+                        <td rowSpan="1">uni</td>
+                        <td rowSpan="1">-</td>
+                        <td>nuez</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="1">13</td>
+                        <td rowSpan="1">uni</td>
+                        <td rowSpan="1">-</td>
+                        <td>almendra</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="1">15</td>
+                        <td rowSpan="1">uni</td>
+                        <td rowSpan="1">-</td>
+                        <td>mani</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="1">25</td>
+                        <td rowSpan="1">uni</td>
+                        <td rowSpan="1">-</td>
+                        <td>avellana</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="1">1</td>
+                        <td rowSpan="1">reb</td>
+                        <td rowSpan="1">-</td>
+                        <td>queso mantecoso</td>
+                    </tr>
+            </tbody>
+            </table>
+        </>
     )
 }
